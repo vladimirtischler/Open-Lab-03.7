121ec9834113d6c9d385208bf88a6ec50ce7a29a
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-26 17:58:26 +0200

    open lab 03.7

---
+++ Open-Lab-03.7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace open_lab_03._7
+{
+    class Program
+    {
+        public static bool EqualSlices(int total, int people, int each)
+        {
+            return (people * each == total);
+        }
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(EqualSlices(a, b, c));
+
+        }
+
+    }
+}
