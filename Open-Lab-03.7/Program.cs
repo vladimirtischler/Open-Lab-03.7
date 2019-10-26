e1de7c9ab5b03a43e9c1c78a9ec703d35c9409c4
vladimirtischler <56106321+vladimirtischler@users.noreply.github.com>
2019-10-26 18:29:35 +0200

    dfnfnc

--- Open-Lab-03.7/Program.cs
+++ Open-Lab-03.7/Program.cs
@@ -6,15 +6,17 @@ namespace open_lab_03._7
     {
         public static bool EqualSlices(int total, int people, int each)
         {
-            return (people * each =< total);
+            return (people * each <= total);
         }
         static void Main(string[] args)
         {
+            Console.WriteLine("enter total");
             int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter people");
             int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter each");
             int c = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(EqualSlices(a, b, c));
-
         }
 
     }
